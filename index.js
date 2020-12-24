dfc0207969471a72240cff60d114ea0739114233
Ramendra Namdev <ramendranamdev@gmail.com>
2020-12-24 17:40:50 +0530

    Process.env.port added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-const HOST_PORT = 3000;
+const HOST_PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(HOST_PORT, () => {
+app.listen(process.env.PORT | HOST_PORT, () => {
   console.log(`Server running on: http://localhost:${HOST_PORT}`);
 });
